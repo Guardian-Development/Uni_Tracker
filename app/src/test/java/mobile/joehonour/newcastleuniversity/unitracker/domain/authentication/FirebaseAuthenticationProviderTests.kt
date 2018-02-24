5253b583a30eae4a526dfc8e0b86b13c28c94172
Joe <joehonour@hotmail.co.uk>
2018-02-24 16:34:39 +0000

    git update

---
+++ app/src/test/java/mobile/joehonour/newcastleuniversity/unitracker/domain/authentication/FirebaseAuthenticationProviderTests.kt
@@ -0,0 +1,19 @@
+package mobile.joehonour.newcastleuniversity.unitracker.domain.authentication
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Created by joehonour on 24/02/2018.
+ */
+class FirebaseAuthenticationProviderTests {
+    @Test
+    fun getUserLoggedIn() {
+    }
+
+    @Test
+    fun authenticateWithTwitterSession() {
+    }
+
+}
---
+++ app/src/test/java/mobile/joehonour/newcastleuniversity/unitracker/domain/extensions/NullHelperExtensionsTests.kt
@@ -0,0 +1,15 @@
+package mobile.joehonour.newcastleuniversity.unitracker.domain.extensions
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Created by joehonour on 24/02/2018.
+ */
+class NullHelperExtensionsTests {
+    @Test
+    fun notNull() {
+    }
+
+}
